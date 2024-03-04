8f544d69d137bdaa3832c8ffbde8907ba6b72555
joganalon <158650228+joganalon@users.noreply.github.com>
2024-03-05 00:00:41 +0800

    Update originalOutput.cpp
    correct errors: 
    1. added the missing return value of 1 in lines 18, 28, 33.
    2. corrected matrix1 to matrix2 in line 23
    3. removed the extra closing bracket in line 36.
    4. corrected calues to values
    5. added output message for product matrix

--- originalOutput.cpp
+++ originalOutput.cpp
@@ -15,29 +15,29 @@ int main(){
 	cin >> row1 >> column1; //enter dimension for matrix1
 	if(row1 > maxValue || column2 > maxValue){
 		cerr << "matrix should not be greater than 6 by 6. " << endl;
-		return;
+		return 1;
 	}
 	
 	double matrix1[row1][column1]; //create the matrix
 	
-	cout << "enter dimension for matrix1: " << endl;
+	cout << "enter dimension for matrix2: " << endl;
 	
 	cin >> row2 >> column2; //enter dimension for matrix2
 	if(row2 > maxValue || column2 > maxValue){
 		cerr << "matrix should not be greater than 6 by 6. " << endl;
-		return;
+		return 1;
 	}
 	
 	if(column1 != row2){ //compatibility check
 		cerr << "matrix incompatible" << endl;
-		return;
+		return 1;
 	}
 
-	double matrix2[row2][column2]]; //create the matrix
+	double matrix2[row2][column2]; //create the matrix
 	
 	double matrix3[row1][column2];  // create the product matrix
 	
-	cout << "enter values for first matrix: " << endl;
+	cout << "enter values for second matrix: " << endl;
 	
 	for(int i = 0; i < row1; i++){ //input data for matrix1
 		for(int j = 0; j < column1; j++){
@@ -45,7 +45,7 @@ int main(){
 		}
 	}
 	
-	cout << "enter calues for second matrix: " << endl;
+	cout << "enter values for second matrix: " << endl;
 	
 	for(int i = 0; i < row2; i++){ //input data for matrix2
 		for(int j = 0; j < column2; j++){
@@ -60,6 +60,8 @@ int main(){
 			}
 		}
 	}
+
+	cout << "product matrix: " << endl;
 	
 	for(int i = 0; i < row2; i++){ //display product matrix
 		for(int j = 0; j < column2; j++){
